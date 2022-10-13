e685cbcb903d9549b75f4a4aa3319902f993a3c2
KevinBouchez <108410273+KevinBouchez@users.noreply.github.com>
2022-10-13 15:42:04 +0200

    Delete site-ug-propre directory

--- site-ug-propre/assets/bdd-boutique.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//connexion à la base de données
-$con = mysqli_connect("localhost","kevin","","UG-Site");
-//verifier la connexion
-if(!$con) die('Erreur : '.mysqli_connect_error());
-
-
-
-?>
